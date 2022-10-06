d33c1bb7e848b25477a816f9d003aeec6f50a521
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-10-06 10:49:34 +0200

    cleanup and demo

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -1,24 +1,100 @@
 <script>
-	import { addMonths as add_months, isSameMonth as is_same_month } from 'date-fns';
-	import Grid from './Grid.svelte';
-	import Header from './Header.svelte';
+	import {
+		addMonths as add_months,
+		getDate as get_date,
+		getDay as get_week_day,
+		getDaysInMonth as get_days_in_month,
+		isSameMonth as is_same_month,
+		isSameYear as is_same_year,
+		setDate as set_date
+	} from 'date-fns';
+
+	export let start_on_sunday = false;
 	let view_date = new Date();
 	let today_date = new Date();
 
+	const WEEK_DAYS = ['M', 'D', 'M', 'D', 'F', 'S', 'S'];
 	const set_next_month = () => (view_date = add_months(view_date, 1));
 	const set_prev_month = () => (view_date = add_months(view_date, -1));
 	const set_today = () => (view_date = new Date(today_date));
+
+	if (start_on_sunday) {
+		const s = WEEK_DAYS.splice(WEEK_DAYS.length - 1, 1);
+		WEEK_DAYS.unshift(s[0]);
+	}
+
+	$: view_month_string = view_date.toLocaleDateString(undefined, { month: 'long' });
+	$: view_year_string = view_date.toLocaleDateString(undefined, { year: 'numeric' });
+	$: view_is_different_year = !is_same_year(view_date, today_date);
 	$: view_is_same_month = is_same_month(view_date, today_date);
+	$: days_in_month = get_days_in_month(view_date);
+	$: first_week_day = get_week_day(set_date(view_date, 1)) || (start_on_sunday ? 0 : 7);
 </script>
 
-<Header
-	{view_date}
-	{today_date}
-	{view_is_same_month}
-	on:today={set_today}
-	on:prev_month={set_prev_month}
-	on:next_month={set_next_month}
-/>
-<Grid {view_date} {today_date} {view_is_same_month} let:date let:is_today>
-	<slot {date} {is_today}>{date}</slot>
-</Grid>
+<div class="header">
+	<span class="month">
+		{view_month_string}
+		{#if view_is_different_year}
+			{view_year_string}
+		{/if}
+	</span>
+
+	<div>
+		<button on:click={set_prev_month} aria-label="Previous Month"> ⬅️ </button>
+		<button on:click={set_today} aria-label="today" disabled={view_is_same_month}> 🏠 </button>
+		<button on:click={set_next_month} aria-label="Next Month"> ➡️ </button>
+	</div>
+</div>
+
+<div class="grid">
+	{#each WEEK_DAYS as day}
+		<div class="week-days">
+			{day}
+		</div>
+	{/each}
+	{#each Array(start_on_sunday ? first_week_day : first_week_day - 1) as _}
+		<div class="cell" />
+	{/each}
+	{#each Array(days_in_month) as _, i}
+		{@const date = set_date(view_date, i + 1)}
+		{@const today = view_is_same_month ? get_date(today_date) === i + 1 : false}
+		<div class="cell">
+			<slot {date} {today}>
+				{date.toLocaleDateString(undefined, { day: 'numeric' })}
+			</slot>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: var(--header-margin, 0 0 1.5rem 0);
+	}
+
+	.month {
+		font-weight: var(--month-font-weight, 600);
+		font-size: var(--month-font-size, 1.5rem);
+	}
+
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		align-items: center;
+		justify-items: center;
+	}
+
+	.week-days {
+		color: var(--week-days-color, gray);
+		font-weight: var(--week-days-font-weight, 600);
+		margin: var(--week-days-margin, 0 0 0.5rem 0);
+	}
+
+	.cell {
+		height: var(--cell-height, clamp(4rem, 7vw, 8rem));
+		text-align: center;
+		width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,88 @@
 <script>
 	import Calendar from '$lib';
+	import { isSameDay as is_same_day } from 'date-fns';
+
+	const data = [
+		{
+			date: new Date('2022-10-01'),
+			color: '#F44336'
+		},
+		{
+			date: new Date('2022-10-02'),
+			color: '#F44336'
+		},
+		{
+			date: new Date('2022-10-03'),
+			color: '#F44336'
+		},
+		{
+			date: new Date('2022-10-20'),
+			color: '#8BC34A'
+		},
+		{
+			date: new Date('2022-11-20'),
+			color: '#8BC34A'
+		}
+	];
 </script>
 
-<Calendar let:date let:is_today>
-	<div class="date" class:is_today>
-		{date}
-	</div>
-	<div class="bar" />
+<Calendar let:date let:today>
+	{@const day = data.find((a) => is_same_day(a.date, date))}
+	<button
+		on:click={() =>
+			alert(
+				`You selected the date ${date.toLocaleDateString(undefined)}.` +
+					(day ? `\nThis date has the color ${day?.color}.` : '') +
+					(today ? `\nThis date is today!` : '')
+			)}
+	>
+		<div class="date" class:today>
+			{date.toLocaleDateString(undefined, { day: 'numeric' })}
+		</div>
+		<div class="dot">
+			{#if day}
+				•
+			{/if}
+		</div>
+		<div class="bar" style:background-color={day?.color || null} />
+	</button>
 </Calendar>
 
 <style>
 	.date {
+		display: grid;
+		place-content: center;
 		border-radius: 99999px;
 		aspect-ratio: 1/1;
 		width: 1.3em;
 		margin: auto;
 	}
-	.is_today {
-		background-color: red;
+
+	.today {
+		background-color: #ff6f00;
 		color: white;
 	}
+
 	.bar {
-		height: 1em;
-		background-color: yellowgreen;
+		height: clamp(0.2rem, 1vw, 3rem);
+	}
+
+	.dot {
+		height: 1.4em;
+	}
+
+	button {
+		width: 100%;
+		height: 100%;
+		border: 0;
+		background-color: white;
+		cursor: pointer;
+		transition: all 0.2s ease-in;
+		border-bottom: 1px solid gray;
+		padding: 0;
+	}
+
+	button:hover {
+		background-color: lightgrey;
 	}
 </style>
